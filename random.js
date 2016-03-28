9ada6ac09dc2d78798f05b0cc91d02276ff2be19
Sergio Garcia Murillo <sergio.garcia.murillo@gmail.com>
2016-03-29 01:31:34 +0200

    Remove not ES5 code

--- random.js
+++ random.js
@@ -255,12 +255,13 @@ if (Meteor.isServer) {
 
 // Create a non-cryptographically secure PRNG with a given seed (using
 // the Alea algorithm)
-Random.createWithSeeds = function (...seeds) {
+/*Random.createWithSeeds = function (...seeds) {
   if (seeds.length === 0) {
     throw new Error("No seeds were provided");
   }
   return new RandomGenerator(RandomGenerator.Type.ALEA, {seeds: seeds});
 };
+*/
 
 // Used like `Random`, but much faster and not cryptographically
 // secure
